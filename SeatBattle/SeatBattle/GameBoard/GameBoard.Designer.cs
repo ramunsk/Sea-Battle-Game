0abd30a870fa650324c8b0aa3de4bbab47b4513d
Ramunas <ramunsk@gmail.com>
2011-11-07 11:40:03 +0200

    Human player grid

---
+++ SeatBattle/SeatBattle/GameBoard/GameBoard.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SeatBattle.CSharp.GameBoard
+{
+    partial class GameBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(243, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GameBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 309);
+            this.Controls.Add(this.label1);
+            this.Name = "GameBoard";
+            this.Text = "GameBoard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SeatBattle/SeatBattle/GameBoard/GameBoard.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace SeatBattle.CSharp.GameBoard
+{
+    public partial class GameBoard : Form
+    {
+        public GameBoard()
+        {
+            InitializeComponent();
+            LayoutHumanPanel();
+        }
+    }
+}
