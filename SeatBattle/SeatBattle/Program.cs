ef0b38a7b0e96006d6f9da0414b23e460ddb6416
Ramunas <ramunsk@gmail.com>
2011-11-07 11:40:31 +0200

    Human player grid (#2)

--- SeatBattle/SeatBattle/Program.cs
+++ SeatBattle/SeatBattle/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
-namespace SeatBattle
+namespace SeatBattle.CSharp
 {
     static class Program
     {
@@ -15,7 +13,7 @@ namespace SeatBattle
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GameBoard.GameBoard());
         }
     }
 }
