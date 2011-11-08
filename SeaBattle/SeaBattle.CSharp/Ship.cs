a42bac3c4dee4ee6e34fa43189951d33aea9eca2
Ramunas <ramunsk@gmail.com>
2011-11-08 16:49:23 +0200

    Initial ship randomization

---
+++ SeaBattle/SeaBattle.CSharp/Ship.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SeatBattle.CSharp
+{
+    public class Ship
+    {
+        public Point Location { get; set; }
+        public int Length { get; set; }
+        public bool IsHorizontal { get; set; }
+
+        public Ship(int length)
+        {
+            Length = length;
+            Location = new Point(-1, -1);
+        }
+    }
+
+    public class RandomPoint
+    {
+        public int X;
+        public int Y;
+
+        public RandomPoint()
+        {
+            var r = new Random(DateTime.Now.Millisecond);
+            X = r.Next(1, 10);
+            Y = r.Next(1, 10);
+        }
+
+        public Point ToPoint()
+        {
+            return new Point(X, Y);
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<Ship> Ships { get; set; }
+
+
+        public Player(string name)
+        {
+            Name = name;
+            Ships = new List<Ship>
+                    {
+                        new Ship(4),
+                        new Ship(3), new Ship(3),
+                        new Ship(2), new Ship(2), new Ship(2),
+                        new Ship(1), new Ship(1), new Ship(1), new Ship(1)
+                    };
+        }
+
+
+        public void RandomizeShips()
+        {
+            foreach(var ship in Ships)
+            {
+                ship.Location = new RandomPoint().ToPoint();
+            }
+        }
+    }
+}
