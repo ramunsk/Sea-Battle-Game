057c5ac9d7506f523f04cd8249e541a489c895cc
Ramunas <ramunsk@gmail.com>
2011-11-08 16:21:03 +0200

    Some namespace shortcuts

--- SeaBattle/SeaBattle.Cpp/GameBoard.cpp
+++ SeaBattle/SeaBattle.Cpp/GameBoard.cpp
@@ -2,7 +2,8 @@
 #include "GameBoard.h"
 #include "PlayerBoard.h"
 
-
+namespace SD = System::Drawing;
+namespace WF = System::Windows::Forms;
 
 namespace SeaBattle {
     namespace Cpp{
@@ -10,6 +11,8 @@ namespace SeaBattle {
         GameBoard::GameBoard(){
             
             this->SuspendLayout();
+
+            this->CreateWindowLayout();
 
             this->_humanBoard = gcnew PlayerBoard();
             this->Controls->Add(this->_humanBoard);
@@ -20,11 +23,11 @@ namespace SeaBattle {
         void GameBoard::CreateWindowLayout(){
             
             this->AutoScaleDimensions = SizeF(8, 19);
-            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+            this->AutoScaleMode = WF::AutoScaleMode::Font;
             this->ClientSize = System::Drawing::Size(800, 500);
-            this->Font = (gcnew System::Drawing::Font("Calibri", 10, FontStyle::Regular, GraphicsUnit::Point, 186));
-            this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
-            this->Text = L"SeaBattle++";       
+            this->Font = (gcnew SD::Font("Calibri", 10, FontStyle::Regular, GraphicsUnit::Point, 186));
+            this->Margin = WF::Padding(4, 4, 4, 4);
+            this->Text = "SeaBattle++";       
         }
     }
 }
